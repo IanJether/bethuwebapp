d5640fe110a45afcffce04f35a983c238ca0e34d
Ian Jether Kimutai <102610860+IanJether@users.noreply.github.com>
2023-04-16 07:53:19 +0300

    Contact us Mobile

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,20 +28,56 @@ const Contact = () => {
                 </div>
             </div>
 
-            <div className="contactboxes px-[17%] flex lg:flex-row lg;justify-between">
-                <div className="boxone">
-                    <div className="boxloc">
-                        {/* <div><FontAwesomeIcon icon={faLocationPin}/></div> */}
-
+            <div className="contactboxes px-[7%] flex flex-col">
+                <div className="boxone flex gap-[7px] w-[100%]">
+                    <div className="boxloc flex flex-col gap-[10px] text-center items-center justify-center shadow-xl w-[50%] h-[130px]">
+                        <div className="text-[25px] text-yellow-300"><FontAwesomeIcon icon={faLocationPin} /></div>
+                        <div className="text-[15px] font-semibold"><h3>Our Location</h3></div>
+                        <div className="text-[12px] text-neutral-400"><p>Chandaria Innovation Center</p></div>
                     </div>
-                    <div className="phonebox">
-
+                    <div className="phonebox flex flex-col gap-[10px] text-center items-center justify-center shadow-xl w-[50%] h-[130px]">
+                        <div className="text-[25px] text-yellow-300"><FontAwesomeIcon icon={faLocationPin} /></div>
+                        <div className="text-[15px] font-semibold"><h3>Our Location</h3></div>
+                        <div className="text-[12px] text-neutral-400"><p>Chandaria Innovation Center</p></div>
                     </div>
 
                 </div>
-                <div className="boxtwo">
+                <div className="boxtwo flex gap-[7px] w-[100%]">
+                    <div className="boxloc flex flex-col gap-[10px] text-center items-center justify-center shadow-xl w-[50%] h-[130px]">
+                        <div className="text-[25px] text-yellow-300"><FontAwesomeIcon icon={faLocationPin} /></div>
+                        <div className="text-[15px] font-semibold"><h3>Our Location</h3></div>
+                        <div className="text-[12px] text-neutral-400"><p>Chandaria Innovation Center</p></div>
+                    </div>
+                    <div className="phonebox flex flex-col gap-[10px] text-center items-center justify-center shadow-xl w-[50%] h-[130px]">
+                        <div className="text-[25px] text-yellow-300"><FontAwesomeIcon icon={faLocationPin} /></div>
+                        <div className="text-[15px] font-semibold"><h3>Our Location</h3></div>
+                        <div className="text-[12px] text-neutral-400"><p>Chandaria Innovation Center</p></div>
+                    </div>
 
                 </div>
+            </div>
+
+
+            <form action="">
+                <div className="form px-[7%] flex flex-col gap-[30px] pt-[70px] pb-[50px]">
+                    <div><input className="border border-neutral-200 w-[100%] h-[45px] px-[10px] text-[12px] outline-none hover:border-yellow-500" placeholder="Enter Name" type="text" /></div>
+                    <div><input className="border border-neutral-200 w-[100%] h-[45px] px-[10px] text-[12px] outline-none hover:border-yellow-500" placeholder="Enter Email" type="text" /></div>
+                    <div><input className="border border-neutral-200 w-[100%] h-[45px] px-[10px] text-[12px] outline-none hover:border-yellow-500" placeholder="Enter Subject" type="text" /></div>
+                    <div><textarea className="border border-neutral-200 w-[100%] h-[130px] p-[10px] text-[12px] outline-none hover:border-yelllow-500" placeholder="Your Message" name="" id="" cols="30" rows="10"></textarea></div>
+                    <div> <button className="bb uppercase">Send message</button></div>
+                </div>
+            </form>
+
+            <div className="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9152.595129544488!2d35.26275755377199!3d0.5180220349639866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x178101ae37f9f535%3A0xe2db337df0fc0820!2sEldoret!5e0!3m2!1sen!2ske!4v1681620604847!5m2!1sen!2ske"
+                    width="100%"
+                    height="350"
+                    style={{border: "0"}}
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+
+                </iframe>
             </div>
 
         </div>
